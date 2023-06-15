eb4266a39c63d54ec0061d234a51120a38d3d750
Angel <angelvargasvelez21@gmail.com>
2023-06-14 22:52:36 -0600

    ya se almacenan los valores del correo y cuerpo

--- src/Correo.js
+++ src/Correo.js
@@ -1,34 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function Correo() {
-
-  const [correo, setCorreo] = useState('')
+  const [correo, setCorreo] = useState("");
+  const [cuerpo, setCuerpo] = useState("");
 
   return (
     <>
-      <form onSubmit={()=>{
-        alert('Correo enviado')
-      }}>
+      <form
+        onSubmit={() => {
+          alert("el valor de correo es: " + correo + '\n y el valor del cuerpo es: ' + cuerpo);
+        }}
+      >
         <div>
           <label>
             Correo:
-            <input
-              type="email"
-              onChange={(e) => {
-                console.log(e.target.value);
-              }}
-            />
+            <input type="email" value={correo} onChange={(e)=>{setCorreo(e.target.value)}} />
           </label>
         </div>
 
         <div>
           <label>
             Cuerpo del correo:
-            <textarea
-              onChange={(e) => {
-                console.log(e.target.value);
-              }}
-            />
+            <textarea value={cuerpo} onChange={(e)=>{setCuerpo(e.target.value)}}/>
           </label>
         </div>
 
